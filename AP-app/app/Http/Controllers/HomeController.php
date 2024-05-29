e7e9bce9d9e3b2deeec2e68ddd7b399a5c291dd2
Joeri Harreman <harr0006@hz.nl>
2024-05-29 22:58:02 +0200

    resotre

---
+++ AP-app/app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // Fetch data from EnappsysController
+        $dataService = new EnappsysController();
+        $data = $dataService->getEnappsysData();
+
+        return view('home', [
+            'optimalTimes' => $data['optimalTimes'],
+            'limitedTimes' => $data['limitedTimes'],
+            'worstTimes' => $data['worstTimes'],
+            'dayType' => $data['dayType'],
+        ]);
+    }
+}
